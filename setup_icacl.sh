7cd23fd2bc57c067d02f636be1916692df576474
D0M3Y <mail2@dbauer.eu>
2023-03-12 15:37:54 +0100

    Fixed typo

--- setup_icacl.sh
+++ setup_icacl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mv /opt/Citrix/ICAClient/keystore/cacerts /opt/Citrix/ICAClient/keystore/cacerts.bakup
+mv /opt/Citrix/ICAClient/keystore/cacerts /opt/Citrix/ICAClient/keystore/cacerts.backup
 ln -s /etc/ssl/certs/ /opt/Citrix/ICAClient/keystore/cacerts
