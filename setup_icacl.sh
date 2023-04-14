3d63173e3ca28e8915adbfc2f4511a4c3128db85
D0M3Y <mail@dbauer.eu>
2023-04-14 16:28:47 +0200

    Added $ICA_KEYSTORE variable with the absolute path to the keystore folder and added the line, where it moves the backup-folder back

--- setup_icacl.sh
+++ setup_icacl.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
+ICA_KEYSTORE=/opt/Citrix/ICAClient/keystore
+
+if [ $2 != "" ]; then
+	echo "Too much arguments!"
+	exit 1
+fi
+
 case $1 in
 	--undo|-u)
-		rm /opt/Citrix/ICAClient/keystore/cacerts
+		rm $ICA_KEYSTORE/cacerts
+		mv $ICA_KEYSTORE/cacerts_backup $ICA_KEYSTORE/cacerts
 		;;
 	--setup|-s)
-		mv /opt/Citrix/ICAClient/keystore/cacerts /opt/Citrix/ICAClient/keystore/cacerts_backup
-		ln -s /etc/ssl/certs/ /opt/Citrix/ICAClient/keystore/cacerts
+		mv $ICA_KEYSTORE/cacerts $ICA_KEYSTORE/cacerts_backup
+		ln -s /etc/ssl/certs/ $ICA_KEYSTORE/cacerts
 		;;
 	--help|-h)
+		echo ""
 		echo "Usage: ./setup_icacl.sh [parameter] (-y)"
 		echo "Setup cacerts link: ./setup_icacl.sh --setup"
 		echo "Undo changes: ./setup_icacl.sh --undo"
 		echo "Show this site: ./setup_icacl.sh --help"
+		echo ""
 		;;
 
 		*)
