c3e8da3b8d427343933e275286fdf253f67171ee
D0M3Y <mail@dbauer.eu>
2023-04-14 16:13:21 +0200

    Converted setup_icacl.sh to an more interactive script

--- setup_icacl.sh
+++ setup_icacl.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-mv /opt/Citrix/ICAClient/keystore/cacerts /opt/Citrix/ICAClient/keystore/cacerts.backup
-ln -s /etc/ssl/certs/ /opt/Citrix/ICAClient/keystore/cacerts
+case $1 in
+	--undo|-u)
+		rm /opt/Citrix/ICAClient/keystore/cacerts
+		;;
+	--setup|-s)
+		mv /opt/Citrix/ICAClient/keystore/cacerts /opt/Citrix/ICAClient/keystore/cacerts_backup
+		ln -s /etc/ssl/certs/ /opt/Citrix/ICAClient/keystore/cacerts
+		;;
+	--help|-h)
+		echo "Usage: ./setup_icacl.sh [parameter] (-y)"
+		echo "Setup cacerts link: ./setup_icacl.sh --setup"
+		echo "Undo changes: ./setup_icacl.sh --undo"
+		echo "Show this site: ./setup_icacl.sh --help"
+		;;
+
+		*)
+		echo "Wrong Usage. Try --help for more information."
+		;;
+esac
+
+
